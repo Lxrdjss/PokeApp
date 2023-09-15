96c34a1a718405189c61519f8effad659cb312e0
Mathias Detrez <lxrdjss@MacBook-Pro-16-de-Mathias.local>
2023-09-15 16:33:31 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const PokedexScreen = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=1200';
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonData(data.results);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.pokemonItem}>
+      <Image
+        style={styles.pokemonImage}
+        source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.url.split('/')[6]}.png` }}
+      />
+      <Text style={styles.pokemonName}>{item.name}</Text>
     </View>
   );
-}
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={pokemonData}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.name}
+        numColumns={2}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
+    padding: 16,
+  },
+  pokemonItem: {
+    flex: 1,
+    margin: 8,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  pokemonImage: {
+    width: 100,
+    height: 100,
+  },
+  pokemonName: {
+    marginTop: 8,
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
+
+export default PokedexScreen;
